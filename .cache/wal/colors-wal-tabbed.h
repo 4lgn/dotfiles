7bdda02d55238c7f361dd8992cd9d55685eea0d7
algn <algn@itu.dk>
2019-02-17 00:15:33 +0100

    Removed redundant crap from VCS

--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#271c1c";
-static const char* selfgcolor   = "#c9c6c6";
-static const char* normbgcolor  = "#446baa";
-static const char* normfgcolor  = "#c9c6c6";
-static const char* urgbgcolor   = "#c7918f";
-static const char* urgfgcolor   = "#c9c6c6";
