3e3282e03781196c3379cffa0beb00d035846691
algn <algn@itu.dk>
2020-02-08 19:54:22 +0100

    Migrated from compton to picom, updated installation script, deleted unused scripts

--- Installation/init_install.sh
+++ Installation/init_install.sh
@@ -2,24 +2,29 @@
 sudo pacman -S xorg xorg-xinit feh rofi ranger rxvt-unicode i3-wm dunst vim neofetch python3 fish python-pip imagemagick terminus-font ttf-font-awesome adobe-source-code-pro-fonts noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra --noconfirm
 
 
-read -p 'Install virtualbox additions? (y/N)' choice
+read -p 'Install virtualbox additions? (y/N): ' choice
 
 if [ "$choice" = "y" ]; then
-    sudo pacman -S xf86-video-vmware virtualbox-guest-utils virtualbox-guest-modules-arch
+    sudo pacman -S xf86-video-vmware virtualbox-guest-utils virtualbox-guest-modules-arch --noconfirm
 fi
 
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --noconfirm
 
 
 yay -S tamzen-font polybar --removemake --nocleanmenu --nodiffmenu --noeditmenu
 
-pip3 install pywal
+sudo pip3 install pywal
 
 cp -rT ~/dotfilesv2 ~
 
-curl https://images.wallpaperscraft.com/image/lupine_flowers_blue_159513_7360x4912.jpg > wallpaper.jpg
-feh --bg-fill wallpaper.jpg
-wal -i wallpaper.jpg
+curl https://images.wallpaperscraft.com/image/lupine_flowers_blue_159513_7360x4912.jpg > ~/wallpaper.jpg
+
+echo "=============="
+echo "==== DONE ===="
+echo "=============="
+echo "$ xstart"
+echo "$ feh --bg-fill ~/wallpaper.jpg"
+echo "$ wal -i ~/wallpaper.jpg"
