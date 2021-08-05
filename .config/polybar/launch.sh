1ff47888a8211563744fa40bdf79364c778356b2
Alexander G. Nielsen <algn@algn.dk>
2021-08-05 13:24:46 +0200

    Better dual-monitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,5 +8,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+# Launch polybar for all monitors
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+done
---
+++ .screenlayout/2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1080 --pos 320x1440 --rotate normal --output DP-1 --off --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output HDMI-2 --off
